215ecd61e8519c4de7e18fa02f6ce0d63f7d9760
Tanya Schlusser <tanya@tickel.net>
2015-02-24 15:28:30 -0600

    added the Python 2.7 verion check

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
-"""Um di dum
-Do di Do
 """
+hello_world.py
+
+This confirms that requests is installed and states
+the version, which should be 2.7
+"""
+import requests
 import sys
 
-print sys.version
+print "Hello World!"
+print "The Python version is:", sys.version
+
+version = sys.version_info
+if version.major != 2 or version.minor != 7:
+    raise(Exception("Version is not 2.7"))
