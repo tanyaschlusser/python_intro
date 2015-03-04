d5a4494322c0cd9b788131603c93b439be7549e9
Tanya Schlusser <tanya@tickel.net>
2015-03-04 13:18:27 -0600

    added .DS_Store to ignore list

---
+++ scratch/__init__.py
@@ -0,0 +1,16 @@
+"""
+This file must exist to make the contents this directory a module.
+
+It imports the contents of files that will be exposed through
+the top level, and usually people put a copyright notice
+(or a reference to a copyright notice at the top level)
+and a list of variables and functions in the package.
+"""
+
+__title__ = 'scratch'
+__version__ = '0.0.0'
+__author__ = 'Tanya "slushy" Schlusser'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Tanya Schlusser'
+
+from .scratch import *
---
+++ scratch/scratch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+scratch.py
+
+scratch place to learn function definition
+"""
+
+def greet_person(person):
+    """Greet the named person.
+
+    This is a docstring -- a comment following
+    the first row of the function declaration.
+    The Style Guide recommends a one-line
+    description followed by an empty line and
+    a longer description. Note the triple quotes.
+    """
+    print 'hello', person
